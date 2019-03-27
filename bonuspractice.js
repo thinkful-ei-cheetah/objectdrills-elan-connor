eb5c751223c5fc2722f8569cd1076aaeb4bb2865
Elan Green <green.elan5@gmail.com>
2019-03-27 16:49:30 -0500

    start work on bonus problems

--- bonuspractice.js
+++ bonuspractice.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query) {
+// first we need to loop through the array of objects so we can compare key/value pairs
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === query.id || arr[i].name === query.name || arr[i].squad === query.squad) {
+      return arr[i];
+    }
+  }
+  return null;
+}
+
+console.log(findOne(HEROES, { id: 3 }));
+
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]
+  }
+};
