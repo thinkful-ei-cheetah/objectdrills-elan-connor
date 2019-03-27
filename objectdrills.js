eba3aa97da2cd3bf7c332fafde5bad4ac0c12f93
Elan Green <green.elan5@gmail.com>
2019-03-27 14:29:04 -0500

    add drill 4

--- objectdrills.js
+++ objectdrills.js
@@ -26,5 +26,33 @@ for (let key in obj) {
 
 // Drill 3
 
-const food = { meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'] }
-console.log(food.measl[3])
+const food = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+
+console.log(food.meals[3]);
+
+// Drill 4
+
+const family = [
+  {
+    name: 'Elan',
+    jobTitle: 'Transcriptionist'
+  },
+  {
+    name: 'Sinikka',
+    jobTitle: 'doctor'
+  },
+  {
+    name: 'John',
+    jobTitle: 'doctor'
+  }
+];
+
+for (let i = 0; i < family.length; i++) {
+  console.log(`${family[i].name}: ${family[i].jobTitle}`);
+}
+
+family.forEach(member => {
+  console.log(`${member.name}: ${member.jobTitle}`);
+});
