a5cee923633592ed25e1c9f9e8d8a58a1091e691
Elan Green <green.elan5@gmail.com>
2019-03-27 16:06:35 -0500

    completed all non-bonus drills!

--- objectdrills.js
+++ objectdrills.js
@@ -65,3 +65,70 @@ family.forEach(member => {
 
 
 // Drill 6
+const cipher = {
+  'a': 1,
+  'b': 2,
+  'c': 3,
+  'd': 4
+};
+
+function decode(str) {
+  const firstChar = str[0];
+  const position = cipher[firstChar];
+  return (position === undefined) ? ' ': str[position];
+}
+
+function decodeWords(strs) {
+  return strs
+    .split(' ')
+    .map(word => decode(word))
+    .join('');
+}
+
+
+
+// drill 7
+function createCharacter(name, nickname, race, origin, attack, defense) {
+  return {
+    name,
+    nickname,
+    race,
+    origin,
+    attack,
+    defense,
+    describe: function() {
+      console.log(`${this.name} is a ${this.race} from ${this.origin}`);
+    },
+    evaluateFight: function(character) {
+      let x = this.attack - character.defense;
+      let y = character.attack - this.defense;
+      return (y > x) ? `Your opponent takes ${x} damage and you receive 0 damage.` : `Your opponent takes ${x} damage and you receive ${y} damage.`;
+    }
+  };
+}
+
+let characters = [
+  createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
+  createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1),
+  createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+  createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8),
+  createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5),
+  createCharacter('Arwen Undomiel', 'arwen', 'half-elf', 'Rivendell', 15, 4)
+];
+
+
+
+
+
+console.log(characters.find(character => {
+  return character.nickname === 'aragorn';
+}).describe());
+// need to access describe
+
+console.log(characters.filter(hobbit => {
+  return hobbit.race === 'Hobbit';
+}));
+
+console.log(characters.filter(character => {
+  return character.attack > 5;
+}));
